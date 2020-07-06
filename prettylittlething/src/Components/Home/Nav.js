5a975dbec2d45947a4c2d74b7341865b84806073
Miley Wright <dishaliekwright@yahoo.com>
2020-07-05 23:54:51 -0400

    currency countries

--- prettylittlething/src/Components/Home/Nav.js
+++ prettylittlething/src/Components/Home/Nav.js
@@ -1,9 +1,45 @@
 import React from "react";
+import { Input } from 'antd';
+import { Menu, Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import { SearchOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+import './Home.css';
+
 
 const Nav = () => {
+    const suffix = (
+        <SearchOutlined
+            style={{
+            fontSize: 16,
+            }}
+        />
+    );
+    const menu = (
+        <Menu>
+          <Menu.Item key="0"> 1st menu item </Menu.Item>
+          <Menu.Item key="1"> 2nd menu item </Menu.Item>
+          <Menu.Item key="3">3rd menu item</Menu.Item>
+        </Menu>
+      );
     return(
-        <div>
-            Nav here 
+        <div className='nav'>
+            <div className='topNav'>
+                <Dropdown overlay={menu} trigger={['click']}>
+                    <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                    <img src={require('../Assets/flag.png')} alt='american flag'/> <DownOutlined />
+                    </a>
+                </Dropdown>
+                <Input
+                    placeholder="input search text"
+                    onSearch={value => console.log(value)}
+                    style={{ width: 200, height: 30, fontSize: 12}}
+                    suffix={suffix}
+                />
+            </div>
+            <div className='bottomNav'>
+
+            </div>
         </div>
     )
 }
