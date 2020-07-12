a767c0f0f6016a025507759f299b8ad853840086
Miley Wright <dishaliekwright@yahoo.com>
2020-07-12 01:37:17 -0400

    section two

--- prettylittlething/src/Components/Home/Home.js
+++ prettylittlething/src/Components/Home/Home.js
@@ -5,7 +5,28 @@ const Home = () => {
     return (
         <div>
             <Nav />
-            Home page
+            <div className='section_three'>
+                <div>UP TO 70% OFF EVERYTHING*</div>
+                <div>+ EXTRA 10% OFF ENTER: HURRY10</div>
+                <div>HURRY! LIMITED TIME ONLY</div>
+                <div className='line-4'>*EXCL BEAUTY</div>
+            </div>
+            <div className='section_four'>
+                <div>BECOMING A SOCIAL BUTTERFLY AGAIN? SHOP NOW</div>
+                <div>WE'VE EXTENDED OUR RETURNS PERIOD TO 60 DAYS</div>
+            </div>
+            <div className='section_five'>
+                <img src='https://cdn-media.prettylittlething.com/bms/media/2020/07/06/kOpZ4PZFplVaKEBh3zBjSqCuf7KYRKNyZWFSVyKA.jpeg' alt='null'/>
+            </div>
+            <div className='section_six'>
+
+            </div>
+            <div className='section_seven'>
+
+            </div>
+            <div className='section_eight'>
+
+            </div>
         </div>
     )
 }
