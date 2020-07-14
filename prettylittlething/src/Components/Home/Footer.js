fc5f02e3069a8980ff2e9efbfa110115cb39a3c9
Miley Wright <dishaliekwright@yahoo.com>
2020-07-14 03:03:01 -0400

    footer

--- prettylittlething/src/Components/Home/Footer.js
+++ prettylittlething/src/Components/Home/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className='footer_grid'>
         <div className='footer_list'>
           <div className='footer_head'>LET US HELP YOU</div>
-          <a href='https://www.prettylittlething.us/faq'>Help</a>
+          <a href='https://www.prettylittlething.us/faq' className='top'>Help</a>
           <a href='https://www.prettylittlething.us/returns-policy'>Returns</a>
           <a href='https://www.prettylittlething.us/size-guide'>Size Guide</a>
           <a href='https://www.prettylittlething.us/delivery-information'>Shipping</a>
@@ -16,7 +16,7 @@ const Footer = () => {
         </div>
         <div className='footer_list'>
           <div className='footer_head'>ABOUT US</div>
-          <a href='https://www.prettylittlething.us/about'>About Us</a>
+          <a href='https://www.prettylittlething.us/about' className='top'>About Us</a>
           <a href='https://www.prettylittlething.us/charity'>Giving Back</a>
           <a href='https://www.prettylittlething.us/diversity'>Diversity</a>
           <a href='https://careers.prettylittlething.com/#_ga=2.174988066.1632988021.1594411873-890133378.1593908096'>Careers</a>
@@ -27,14 +27,14 @@ const Footer = () => {
         </div>
         <div className='footer_list'>
           <div className='footer_head'>INFORMATION</div>
-          <a href='https://www.prettylittlething.us/terms'>Terms & Conditions</a>
+          <a href='https://www.prettylittlething.us/terms' className='top'>Terms & Conditions</a>
           <a href='https://www.prettylittlething.us/privacy-policy'>Privacy Policy</a>
           <a href='https://www.prettylittlething.us/privacy-policy#ccpa'>California Consumer Privacy Act</a>
           <a href='https://www.prettylittlething.us/site-map'>Sitemap</a>
         </div>
         <div className='footer_list'>
           <div className='footer_head'>MY ACCOUNT</div>
-          <a href='https://www.prettylittlething.us/sales/order/history'>Order History</a>
+          <a href='https://www.prettylittlething.us/sales/order/history' className='top'>Order History</a>
           <a href='https://www.prettylittlething.us/#tracking_popup'>Track My Order</a>
           <a href='https://www.prettylittlething.us/wishlist'>My Wishlist</a>
           <a href='https://www.prettylittlething.us/checkout/cart/'>View Bag</a>
